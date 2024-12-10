1ec0855d037ea3f3ea64431c788f5fcbb23c9d08
Frans Filasta Pratama <fransfilastap@live.com>
2024-12-10 19:47:22 +0700

    Add new poem content and adjust style in article page
    Added the poem "Kembalikan Indonesia Padaku" with its metadata to the MDX file and enhanced text styling on the article page to improve readability. The changes include setting up the prose and code colors for a better contrast and aesthetic consistency.

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,6 +1,6 @@
-import { allPosts } from "@/lib/content";
-import { Metadata, ResolvingMetadata } from "next";
-import { notFound } from "next/navigation";
+import {allPosts} from "@/lib/content";
+import {Metadata, ResolvingMetadata} from "next";
+import {notFound} from "next/navigation";
 
 export async function generateStaticParams() {
   const posts = await allPosts();
@@ -47,7 +47,7 @@ export default async function PostPage(
 
   return (
     <section className="py-6 xcontainer">
-      <article className="mx-auto prose md:prose-md lg:prose-md xl:prose-md text-foreground prose-headings:text-accent-foreground prose-blockquote:text-muted-foreground prose-a:text-blue-500">
+      <article className="mx-auto prose md:prose-md lg:prose-md xl:prose-md text-foreground prose-headings:text-accent-foreground prose-blockquote:text-muted-foreground prose-a:text-blue-500 prose-code:text-white prose-bold:text-white">
         <h1 className={"font-heading"}>{post.metadata.title}</h1>
         <span className="text-muted-foreground font-light text-sm">{ post.metadata.date.toDateString() }</span>
         {post.content}
